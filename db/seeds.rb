da84665141f3950059904c69692ab49d26faf883
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 00:14:56 +0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: 'click click bang', description: 'When Kev, a super talented footballers dream come crushing down, he is left with no choice but to join the criminal world.
+In the process of trying to win the battle, he loses the war.', movie_length: '2 hrs', director: 'phil karanja', image_url: 'https://www.kenyanvibe.com/wp-content/uploads/2022/06/Click-Click-Bang.jpg')
+Movie.create(title: 'Nairobi half life', description: ' Small-town boy chases big-city dreams. Big city bends him over and tosses him in the gutter. Boy falls in with wrong crowd as a means of survival.', movie_length: '1 hr', director: 'David Kitonga', image_url: 'http://on-the-couch.com/home/wp-content/uploads/2012/09/nhl1.jpg')
+Movie.create(title: 'Nafsi', description: 'A friendship is put in jeopardy when a mutual agreement goes south', movie_length: '1 hr 30 min', director: 'Reuben odanga', image_url: 'https://lh3.googleusercontent.com/W9N-jyKW5pY4pSb7uBctceMEdbDBRlUNVhB1t1pnjPFvNa5guXEmOgrYfaymUjlDbjjfshQKh42O07djzOXLIP5uhULObCc=s1200')
+Movie.create(title: 'Tera Storm', description: 'A team of superheroes unites in an attempt to vanquish an ancient wizard who threatens to destroy the earth with a powerful mysterious artifact.', movie_length: '2 hrs', director: 'Andrew Kaggia', image_url: 'https://upload.wikimedia.org/wikipedia/en/b/bb/TeraStorm_poster.jpg')
+Movie.create(title: 'Mission to rescue', description: 'Along the Kenya- Somali border, the Kenya Special Operations Forces who are training for their next mission receive word that the Assistant County Commissioner and two others have been killed', movie_length: '1 hr 25 mins', director: 'Gilber Lukalia', image_url: 'https://upload.wikimedia.org/wikipedia/en/5/5d/Mission_to_Rescue_poster.jpg')
+Movie.create(title: 'shimoni', description: 'A teacher newly released from prison renegotiates the confines of the physical world while forced to face his nightmare in the flesh.', movie_length: '2 hrs 10 mins', director: 'Angela wamai', image_url: 'https://upload.wikimedia.org/wikipedia/en/3/3b/The_Good_Lie_poster.jpg')
+Movie.create(title: 'Rafiki', description: '"Good Kenyan girls become good Kenyan wives," but Kena and Ziki long for something more. When love blossoms between them, the two girls will be forced to choose between happiness and safety.', movie_length: '2 hrs', director: 'wanuri kairu', image_url: 'https://upload.wikimedia.org/wikipedia/en/9/9c/Rafiki_poster.jpg')
+Movie.create(title: 'soul boy', description: 'A Kenyan boy goes on a strange journey to return his fathers soul.', movie_length: '1 hr 59 minutes', director: 'Hawa essuman', image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Soul_Boy_Poster.png/220px-Soul_Boy_Poster.png')
+Movie.create(title: 'just in time', description: 'A meticulous clerk for a bookstore that has just been shuttered suddenly finds herself taking care of her cousins witty 11-year-old daughter.', movie_length: '1 hr', director: 'lowladee omolola', image_url: 'https://upload.wikimedia.org/wikipedia/en/4/4b/In_Time_poster.jpg')
+
+
