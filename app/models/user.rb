81cbf8ab0329ba384905de2eaa52a2feed56a3be
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-06 22:37:11 +0300

    edit models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    has_secure_password
+    has_many :reviews
     
+    has_secure_password
+
     validates :username, presence: true, uniqueness: true
+    validates :username, length: {minimum: 5}
 end
