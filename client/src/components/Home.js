40ccc7fae65f8ccf31d5399082cc3c66dee012ea
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-08 00:20:58 +0300

    add movie card to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState }  from "react"
 import Login from './Login';
 import Nav from 'react-bootstrap/Nav';
 import { useNavigate } from 'react-router-dom';
-
+import {  Container, Row, Col, } from "react-bootstrap"
+import MovieCard from './MovieCard'
 
 const Home = ({user, setUser}) => {
   const history = useNavigate()
+  const [movies, setMovies] = useState([]);
 
   function handleLogoutClick() {
     history(`/login`)
@@ -16,6 +18,13 @@ const Home = ({user, setUser}) => {
     });
     
 } 
+
+
+useEffect(() => {
+  fetch("/movies")
+    .then((r) => r.json())
+    .then((movies) => setMovies(movies));
+}, []);
 
   console.log(user);
   if (!user) return <Login />;
@@ -37,6 +46,28 @@ const Home = ({user, setUser}) => {
 
       </Nav.Item>
     </Nav>
+
+    <div className='Home text-center'>
+           <p className='text-center  fw-bold title'>
+            Hit <br/> or <br/>Miss
+           </p> 
+           <p className=' find text-center fs-3'>
+              review your favorite kenyan movies
+           </p> 
+          
+          </div>
+
+          <Container>
+             <Row>
+             {movies.map((movie) => (
+            <Col xs={12} md={4}className='p-5'>
+            <MovieCard  movie={movie} key={movie.id}/>
+            </Col>
+             ))}
+            
+
+            </Row>
+          </Container>
       
     </div>
   )
