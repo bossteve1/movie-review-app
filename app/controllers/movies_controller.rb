aaf5821024596aa136b1cc95dcaafbccf010697f
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 08:54:13 +0300

    add show to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,12 @@ class MoviesController < ApplicationController
         movies = Movie.all
         render json:movies, status: :ok
     end
+
+    def show 
+        movie = Movie.find_by(id: params[:id])
+        if movie
+            render json: movie
+        else  render json: { error: "Movie not found" }, status: :not_found
+    end
+    end
 end
