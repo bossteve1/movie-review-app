6220b895f33e1010075e4731ae5dcc54e95c265c
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 09:07:13 +0300

    add show to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+    def index 
+        reviews = Review.all
+        render json:reviews, status: :ok
+    end
+
+    def show 
+        review =Review.find_by(id: params[:id])
+        if review
+            render json: review
+        else render json: { error: "Review not found" }, status: :not_found
+    end
+    end
+
 end
