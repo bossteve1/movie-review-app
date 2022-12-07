05ea9964878c0ec6b81e2b89e90cea6544a89901
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 09:15:12 +0300

    add create in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,22 @@ class ReviewsController < ApplicationController
     end
 
     def show 
-        review =Review.find_by(id: params[:id])
+        review = Review.find_by(id: params[:id])
         if review
             render json: review
         else render json: { error: "Review not found" }, status: :not_found
     end
     end
 
+    def create 
+        review =Review.create!(params_review)
+        render json: review
+    end
+
+
+
+    private 
+    def params_review
+        params.permit(:comments, :user_id, :movie_id, :rating)
+    end
 end
