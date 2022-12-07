8b2ed507ba67c082df6dcf1dadf4d3a90815a346
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 11:06:47 +0300

    edit serializers

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,4 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :movie_length, :director, :image_url
+  attributes :id, :title, :description, :movie_length, :director, :image_url, 
+  has_many :reviews
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :comments, :user_id, :movie_id, :rating
+  belongs_to :movie
+  belongs_to :user
 end
