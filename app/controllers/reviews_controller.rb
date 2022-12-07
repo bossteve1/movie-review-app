a44ec8b953a2b9236b582ccfec0d8de06d27261d
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-12-07 09:27:56 +0300

    add update to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,14 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index 
         reviews = Review.all
         render json:reviews, status: :ok
     end
 
     def show 
-        review = Review.find_by(id: params[:id])
-        if review
-            render json: review
-        else render json: { error: "Review not found" }, status: :not_found
-    end
+        review = find_review
+        render json: review
     end
 
     def create 
@@ -17,10 +16,31 @@ class ReviewsController < ApplicationController
         render json: review
     end
 
+    def update 
+        review = find_review
+        review.update!(params_description)
+        render json: review
+    end
+
+
+
 
 
     private 
+
+    def find_review
+        Review.find_by(id: params[:id])
+    end
+
     def params_review
         params.permit(:comments, :user_id, :movie_id, :rating)
+    end
+
+    def params_description 
+        params.permit(:comments, :rating)
+    end
+
+    def render_not_found_response
+        render json: { errors: "Review not found" }, status: :not_found
     end
 end
